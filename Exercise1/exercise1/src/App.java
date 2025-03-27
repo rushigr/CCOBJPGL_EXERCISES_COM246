7b00aa61749962114f3f378296a78f0f187a6981
Daniel Angelo S. Santos <danielangelosantos2134@gmail.com>
2025-03-27 14:23:53 +0800

    Add files via upload

---
+++ Exercise1/exercise1/src/App.java
@@ -0,0 +1,53 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String username_free_input = scanner.nextLine();
+       
+        System.out.print("Enter your password: ");
+        String username_password = scanner.nextLine();
+
+        System.out.println("Your username is: "+ username_free_input);
+        System.out.println("Your username is: "+ username_password);
+
+        User me = new User(username_free_input, username_password);
+
+        File myFile = new File("Accounts.txt");
+
+        if (myFile.exists()) {
+            System.out.println("File Found");
+        } else {
+            System.out.println("File does not exist");
+        }
+         
+        Scanner fileScanner = new Scanner(myFile);
+
+        Boolean accountexists = false;
+        while (fileScanner.hasNextLine()) {
+            String filedata = fileScanner.nextLine();
+            //System.out.println(filedata);
+
+            String username = filedata.split(",")[0];
+            //System.out.println(username);
+
+            String password = filedata.split(",")[1];
+            //System.out.println(password);
+
+            if (username.equals(username_free_input) && password.equals(username_password)) {
+                accountexists = true;
+                break;
+            }
+        }
+            if(accountexists){
+                System.out.println("Login successful, Hello "+ me.getUsername());
+            }
+            else{
+                System.out.println("Account does not exist");
+            }
+        scanner.close();
+    }
+}
